8e07a7061cabcee94fc8a96491992199f0f60d9f
Agos Masut <agosmasut98@gmail.com>
2021-06-27 18:11:46 -0300

    proyecto base

---
+++ LolaWebApp-Base.WebUi/LolaApp.DataAccess/Repositories/ExampleRepository.cs
@@ -0,0 +1,20 @@
+﻿using LolaApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using LolaApp.DataAccess.Concrete;
+using System.Linq.Expressions;
+
+namespace LolaApp.DataAccess.Repositories
+{
+    public class ExampleRepository : RepositoryBase<Example>, IExampleRepository 
+    {
+        public ExampleRepository(DbContext repositoryContext) : base(repositoryContext)
+        {
+
+        }
+    }
+}
---
+++ LolaWebApp-Base.WebUi/LolaWebApp-Base.WebUi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LolaWebApp_Base.WebUi.MvcApplication" Language="C#" %>
