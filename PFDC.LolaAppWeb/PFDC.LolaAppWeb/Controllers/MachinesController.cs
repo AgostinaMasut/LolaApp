b742721be0dd2461264118f1fb451d77cf52b443
Guillermo Masullo <guimasullo@gmail.com>
2021-08-12 18:38:42 -0300

    camilla y maquinas

---
+++ PFDC.LolaAppWeb/PFDC.LolaAppWeb/Controllers/MachinesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PFDC.LolaAppWeb.Data;
+using PFDC.LolaAppWeb.Models;
+
+namespace PFDC.LolaAppWeb.Controllers
+{
+    public class MachinesController : Controller
+    {
+        private PFDCLolaAppWebContext db = new PFDCLolaAppWebContext();
+
+        // GET: Machines
+        public ActionResult Index()
+        {
+            return View(db.Machine.ToList());
+        }
+
+        // GET: Machines/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Machine machine = db.Machine.Find(id);
+            if (machine == null)
+            {
+                return HttpNotFound();
+            }
+            return View(machine);
+        }
+
+        // GET: Machines/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Machines/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Description")] Machine machine)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Machine.Add(machine);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(machine);
+        }
+
+        // GET: Machines/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Machine machine = db.Machine.Find(id);
+            if (machine == null)
+            {
+                return HttpNotFound();
+            }
+            return View(machine);
+        }
+
+        // POST: Machines/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Description")] Machine machine)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(machine).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(machine);
+        }
+
+        // GET: Machines/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Machine machine = db.Machine.Find(id);
+            if (machine == null)
+            {
+                return HttpNotFound();
+            }
+            return View(machine);
+        }
+
+        // POST: Machines/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Machine machine = db.Machine.Find(id);
+            db.Machine.Remove(machine);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
