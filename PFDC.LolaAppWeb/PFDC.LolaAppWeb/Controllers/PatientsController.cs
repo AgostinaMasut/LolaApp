03bada06750f3df76212d77b83e36954df777c92
Guillermo Masullo <guimasullo@gmail.com>
2021-08-19 18:31:18 -0300

    controllers,Suc,Pac,Trat,ListPre

---
+++ PFDC.LolaAppWeb/PFDC.LolaAppWeb/Controllers/PatientsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PFDC.LolaAppWeb.Data;
+using PFDC.LolaAppWeb.Models;
+
+namespace PFDC.LolaAppWeb.Controllers
+{
+    public class PatientsController : Controller
+    {
+        private PFDCLolaAppWebContext db = new PFDCLolaAppWebContext();
+
+        // GET: Patients
+        public ActionResult Index()
+        {
+            return View(db.Patient.ToList());
+        }
+
+        // GET: Patients/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Patient patient = db.Patient.Find(id);
+            if (patient == null)
+            {
+                return HttpNotFound();
+            }
+            return View(patient);
+        }
+
+        // GET: Patients/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Patients/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,LastName,DNI,SexId,Birthday,Street,StreetNumber,Neighbourhood,ZipCode,City,Mobile,Email")] Patient patient)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Patient.Add(patient);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(patient);
+        }
+
+        // GET: Patients/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Patient patient = db.Patient.Find(id);
+            if (patient == null)
+            {
+                return HttpNotFound();
+            }
+            return View(patient);
+        }
+
+        // POST: Patients/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,LastName,DNI,SexId,Birthday,Street,StreetNumber,Neighbourhood,ZipCode,City,Mobile,Email")] Patient patient)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(patient).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(patient);
+        }
+
+        // GET: Patients/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Patient patient = db.Patient.Find(id);
+            if (patient == null)
+            {
+                return HttpNotFound();
+            }
+            return View(patient);
+        }
+
+        // POST: Patients/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Patient patient = db.Patient.Find(id);
+            db.Patient.Remove(patient);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ PFDC.LolaAppWeb/PFDC.LolaAppWeb/Controllers/TreatmentsController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PFDC.LolaAppWeb.Data;
+using PFDC.LolaAppWeb.Models;
+
+namespace PFDC.LolaAppWeb.Controllers
+{
+    public class TreatmentsController : Controller
+    {
+        private PFDCLolaAppWebContext db = new PFDCLolaAppWebContext();
+
+        // GET: Treatments
+        public ActionResult Index()
+        {
+            var treatment = db.Treatment.Include(t => t.Frequency).Include(t => t.Machine).Include(t => t.Stretcher);
+            return View(treatment.ToList());
+        }
+
+        // GET: Treatments/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Treatment treatment = db.Treatment.Find(id);
+            if (treatment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(treatment);
+        }
+
+        // GET: Treatments/Create
+        public ActionResult Create()
+        {
+            ViewBag.FrequencyId = new SelectList(db.Frequency, "Id", "Id");
+            ViewBag.MachineId = new SelectList(db.Machine, "Id", "Description");
+            ViewBag.StretcherId = new SelectList(db.Stretcher, "Id", "Descrption");
+            return View();
+        }
+
+        // POST: Treatments/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Description,AmountSession,DurationSession,FrequencyId,MachineId,StretcherId")] Treatment treatment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Treatment.Add(treatment);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.FrequencyId = new SelectList(db.Frequency, "Id", "Id", treatment.FrequencyId);
+            ViewBag.MachineId = new SelectList(db.Machine, "Id", "Description", treatment.MachineId);
+            ViewBag.StretcherId = new SelectList(db.Stretcher, "Id", "Descrption", treatment.StretcherId);
+            return View(treatment);
+        }
+
+        // GET: Treatments/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Treatment treatment = db.Treatment.Find(id);
+            if (treatment == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.FrequencyId = new SelectList(db.Frequency, "Id", "Id", treatment.FrequencyId);
+            ViewBag.MachineId = new SelectList(db.Machine, "Id", "Description", treatment.MachineId);
+            ViewBag.StretcherId = new SelectList(db.Stretcher, "Id", "Descrption", treatment.StretcherId);
+            return View(treatment);
+        }
+
+        // POST: Treatments/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Description,AmountSession,DurationSession,FrequencyId,MachineId,StretcherId")] Treatment treatment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(treatment).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.FrequencyId = new SelectList(db.Frequency, "Id", "Id", treatment.FrequencyId);
+            ViewBag.MachineId = new SelectList(db.Machine, "Id", "Description", treatment.MachineId);
+            ViewBag.StretcherId = new SelectList(db.Stretcher, "Id", "Descrption", treatment.StretcherId);
+            return View(treatment);
+        }
+
+        // GET: Treatments/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Treatment treatment = db.Treatment.Find(id);
+            if (treatment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(treatment);
+        }
+
+        // POST: Treatments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Treatment treatment = db.Treatment.Find(id);
+            db.Treatment.Remove(treatment);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
