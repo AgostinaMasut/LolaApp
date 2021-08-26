8cb37e054bfa246623032a34231b59b48696c5da
Guillermo Masullo <guimasullo@gmail.com>
2021-08-26 11:14:10 -0300

    controlador Hist.Med.Corp

---
+++ PFDC.LolaAppWeb/PFDC.LolaAppWeb/Controllers/BodyMeasurementsHistoriesController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PFDC.LolaAppWeb.Data;
+using PFDC.LolaAppWeb.Models;
+
+namespace PFDC.LolaAppWeb.Controllers
+{
+    public class BodyMeasurementsHistoriesController : Controller
+    {
+        private PFDCLolaAppWebContext db = new PFDCLolaAppWebContext();
+
+        // GET: BodyMeasurementsHistories
+        public ActionResult Index()
+        {
+            var bodyMeasurementsHistory = db.BodyMeasurementsHistory.Include(b => b.MeasurementsBodyType).Include(b => b.Patient);
+            return View(bodyMeasurementsHistory.ToList());
+        }
+
+        // GET: BodyMeasurementsHistories/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BodyMeasurementsHistory bodyMeasurementsHistory = db.BodyMeasurementsHistory.Find(id);
+            if (bodyMeasurementsHistory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bodyMeasurementsHistory);
+        }
+
+        // GET: BodyMeasurementsHistories/Create
+        public ActionResult Create()
+        {
+            ViewBag.MeasurementsBodyTypeId = new SelectList(db.MeasurementsBodyType, "Id", "Description");
+            ViewBag.PatientId = new SelectList(db.Patient, "Id", "Name");
+            return View();
+        }
+
+        // POST: BodyMeasurementsHistories/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,MeasurementDate,PatientId,MeasurementsBodyTypeId,CmGr")] BodyMeasurementsHistory bodyMeasurementsHistory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.BodyMeasurementsHistory.Add(bodyMeasurementsHistory);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MeasurementsBodyTypeId = new SelectList(db.MeasurementsBodyType, "Id", "Description", bodyMeasurementsHistory.MeasurementsBodyTypeId);
+            ViewBag.PatientId = new SelectList(db.Patient, "Id", "Name", bodyMeasurementsHistory.PatientId);
+            return View(bodyMeasurementsHistory);
+        }
+
+        // GET: BodyMeasurementsHistories/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BodyMeasurementsHistory bodyMeasurementsHistory = db.BodyMeasurementsHistory.Find(id);
+            if (bodyMeasurementsHistory == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MeasurementsBodyTypeId = new SelectList(db.MeasurementsBodyType, "Id", "Description", bodyMeasurementsHistory.MeasurementsBodyTypeId);
+            ViewBag.PatientId = new SelectList(db.Patient, "Id", "Name", bodyMeasurementsHistory.PatientId);
+            return View(bodyMeasurementsHistory);
+        }
+
+        // POST: BodyMeasurementsHistories/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,MeasurementDate,PatientId,MeasurementsBodyTypeId,CmGr")] BodyMeasurementsHistory bodyMeasurementsHistory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(bodyMeasurementsHistory).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MeasurementsBodyTypeId = new SelectList(db.MeasurementsBodyType, "Id", "Description", bodyMeasurementsHistory.MeasurementsBodyTypeId);
+            ViewBag.PatientId = new SelectList(db.Patient, "Id", "Name", bodyMeasurementsHistory.PatientId);
+            return View(bodyMeasurementsHistory);
+        }
+
+        // GET: BodyMeasurementsHistories/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BodyMeasurementsHistory bodyMeasurementsHistory = db.BodyMeasurementsHistory.Find(id);
+            if (bodyMeasurementsHistory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bodyMeasurementsHistory);
+        }
+
+        // POST: BodyMeasurementsHistories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            BodyMeasurementsHistory bodyMeasurementsHistory = db.BodyMeasurementsHistory.Find(id);
+            db.BodyMeasurementsHistory.Remove(bodyMeasurementsHistory);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
